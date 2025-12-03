25dabc5dedfabe473ab64f74e825ea6d009a1f7f
Qypol342 <qypol342@gmail.com>
2025-12-03 21:16:55 +0100

    init

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for hf_lora_loader."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Loan Maeght"""
+__email__ = "qypol342@gmail.com"
+__version__ = "0.0.1"
+
+from .src.hf_lora_loader.nodes import NODE_CLASS_MAPPINGS
+from .src.hf_lora_loader.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for hf_lora_loader."""
